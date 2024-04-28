dea07a9816cfadded1e6f25d6f97420b01212948
Biswajit Basak <juelbasak@gmail.com>
2024-04-28 20:10:13 +0530

    Added padding

--- 02-bootstrap.py
+++ 02-bootstrap.py
@@ -5,6 +5,8 @@ import ttkbootstrap as ttk
 def btn_func():
     print('Button Pressed!')
 
+def print_hello():
+    third_label_text.set('Hello World!')
 
 window = ttk.Window(themename='journal')
 window.title(string='Window and Widgets')
@@ -12,7 +14,7 @@ window.geometry('600x800')
 
 # ttk label
 top_label = ttk.Label(master=window, text='This is a text')
-top_label.pack()
+top_label.pack(pady=5)
 
 # tk text
 text_area = tk.Text(master=window,
@@ -20,19 +22,27 @@ text_area = tk.Text(master=window,
                     width=65)
 text_area.insert(index='1.0', 
                  chars='This is a sample line\nThis is 2nd line')
-text_area.pack()
+text_area.pack(pady=5)
 
 # tk entry
 
 second_label_text = tk.StringVar(value='one line of text')
 second_label = ttk.Entry(master=window, 
                          textvariable=second_label_text)
-second_label.pack()
+second_label.pack(pady=5)
 
+third_label_text = tk.StringVar(master=window, 
+                                value='')
+third_label = ttk.Label(master=window,
+                        textvariable=third_label_text)
+third_label.pack(pady=5)
+
+button2 = ttk.Button(text='Button', command=print_hello)
+button2.pack(pady=5)
 # ttk button
 
 button = ttk.Button(text='A button',
                     command=btn_func)
-button.pack()
+button.pack(pady=5)
 
 window.mainloop()
