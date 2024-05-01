1d8b2639aeb95cb7443f773f422410923f055fe6
Biswajit Basak <juelbasak@gmail.com>
2024-05-01 18:12:41 +0530

    textvariables

---
+++ 04-bootstrap.py
@@ -0,0 +1,41 @@
+# import tkinter as tk
+from  tkinter import ttk
+import ttkbootstrap as tk
+
+def button_func():
+    print(string_var.get())
+    string_var.set('Button Pressed')
+# window
+window = tk.Window()
+window.title('Tkinter Variables')
+window.geometry('600x500')
+
+# tkinter vairbale
+string_var = tk.StringVar(value='<empty>')
+
+# widgets
+label = ttk.Label(master=window, text='label', textvariable=string_var)
+label.pack()
+
+entry = ttk.Entry(master=window, textvariable=string_var)
+entry.pack(pady=5)
+
+entry2 = ttk.Entry(master=window, textvariable=string_var)
+entry2.pack(pady=5)
+
+button = ttk.Button(master=window, text='button', command=button_func)
+button.pack()
+
+string_var_2 = tk.StringVar(value='Empty Text')
+
+entry_1 = ttk.Entry(master=window, textvariable=string_var_2)
+entry_1.pack()
+
+entry_2 = ttk.Entry(master=window, textvariable=string_var_2)
+entry_2.pack()
+
+label_1 = ttk.Label(master=window, textvariable=string_var_2)
+label_1.pack()
+
+# run
+window.mainloop()
