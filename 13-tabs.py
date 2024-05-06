5059a288cb3cf5c563c965e05c72b1017e4c3616
Biswajit Basak <juelbasak@gmail.com>
2024-05-06 17:25:34 +0530

    notebooks

---
+++ 13-tabs.py
@@ -0,0 +1,44 @@
+import tkinter as tk
+from tkinter import ttk
+
+# Window
+window = tk.Tk()
+window.title('Tabs')
+window.geometry('500x600')
+
+notebook = ttk.Notebook(master=window)
+
+tab1 = ttk.Frame(master=notebook)
+l1 = ttk.Label(master=tab1, text='Home')
+l1.pack()
+
+btn1 = ttk.Button(master=tab1, text='Enter')
+btn1.pack()
+
+tab2 = ttk.Frame(master=notebook)
+l2 = ttk.Label(master=tab2, text='About')
+l2.pack()
+
+btn2 = ttk.Button(master=tab2, text='Exit')
+btn2.pack()
+
+notebook.add(tab1, text='Home')
+notebook.add(tab2, text='About')
+
+tab3 = ttk.Frame(master=notebook)
+
+btn3 = ttk.Button(master=tab3, text='More!')
+btn3.pack()
+
+btn4 = ttk.Button(master=tab3, text='More Data!')
+btn4.pack()
+
+l3 = tk.Label(master=tab3, text='Label')
+l3.pack()
+
+notebook.add(tab3, text='Info')
+notebook.pack()
+
+# Run
+window.mainloop()
+
