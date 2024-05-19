e9c528cbc9f8c2710e19f777f820c565a40efe85
Biswajit Basak <juelbasak@gmail.com>
2024-05-19 15:52:02 +0530

    multiple window

---
+++ 26-responsive-design.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import ttk
+
+# Window
+window = tk.Tk()
+window.title('Responsive Design')
+window.geometry('900x600')
+
+# Run
+window.mainloop()
---
+++ 28-multiple-windows.py
@@ -0,0 +1,37 @@
+import tkinter as tk
+from tkinter import ttk
+
+# Window
+class SubWindow(tk.Toplevel):
+    def __init__(self, title, height, width):
+        super().__init__()
+        self.title(title)
+        self.geometry(f'{width}x{height}')
+
+class App(tk.Tk):
+    def __init__(self, title, height, width):
+        super().__init__()
+        self.title(title)
+        self.geometry(f'{width}x{height}')
+        self.sub_window = None
+
+    def widgets(self, title, height, width):
+        def create_window():
+            if self.sub_window is None:
+                self.sub_window = SubWindow(title, height, width)
+
+        def destroy_window():
+            if self.sub_window is not None:
+                self.sub_window.destroy()
+                self.sub_window = None
+
+        ttk.Button(text='Create a New Window', command=create_window).pack(pady=20)
+        ttk.Button(text='Close the new Window', command=destroy_window).pack()
+
+
+
+app = App(title='Multiple Windows', height=600, width=900)
+app.widgets(title='Sub Window', height=500, width=500)
+
+# Run
+app.mainloop()
